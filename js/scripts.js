1b568929e7766a9e51cf59b35572e7381f7aff10
Xhamps <xhamps@gmail.com>
2016-04-15 01:00:28 -0300

    Ajax no imput do mailchimp

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,17 @@ $('.video-player').click(function() {
         player.addClass('playing');
     }
 });
+
+$.ajaxChimp.translations['pt-BR'] = {
+    'submit': 'Enviando...',
+    0: 'Enviamos um email de confirmação para o seu email.',
+    1: 'Por favor coloque um email valido.',
+    2: 'O e-mail deve conter um único @',
+    3: 'Parte final do email é inválido (a parte após o @: )',
+    4: 'Parte inicial do email é inválido(a parte antes do @: )',
+    5: 'Este endereço de email parece inválido. Insira um endereço de email real'
+};
+
+$("#newsletter-subscribe").ajaxChimp({
+    language: 'pt-BR'
+});
