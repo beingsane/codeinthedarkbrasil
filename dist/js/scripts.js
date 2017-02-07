febef52bb307707761ad7991c6dbdab5f64620b8
Eduardo Matos <eduardoj.matos@gmail.com>
2017-02-07 11:06:56 -0200

    Fixing translation of ajaxChimp

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 $.ajaxChimp.translations['pt-BR'] = {
     'submit': 'Enviando...',
-    0: 'Enviamos um email de confirmação para o seu email.',
+    0: 'Enviamos uma mensagem de confirmação para o seu email.',
     1: 'Por favor coloque um email valido.',
     2: 'O e-mail deve conter um único @',
     3: 'Parte final do email é inválido (a parte após o @: )',
